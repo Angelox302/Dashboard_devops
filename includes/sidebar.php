125dc90fe3ae6796a10b10af6e20a45dffe4dc6f
Jose Daniel <cantillocordobajose@litoral.edu.co>
2026-09-17 13:18:02 -0500

    Separar dashboard en includes

---
+++ includes/sidebar.php
@@ -0,0 +1,23 @@
+<aside class="sidebar">
+
+    <h2>Menú</h2>
+
+    <ul>
+        <li>
+            <a href="dashboard.php">Dashboard</a>
+        </li>
+
+        <li>
+            <a href="#">Productos</a>
+        </li>
+
+        <li>
+            <a href="#">Categorías</a>
+        </li>
+
+        <li>
+            <a href="#">Usuarios</a>
+        </li>
+    </ul>
+
+</aside>
